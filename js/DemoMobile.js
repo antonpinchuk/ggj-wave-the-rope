98de9066dc594f82f25c4cfa2c1ef98dee924a52
Anton Pinchuk <antonpinchuk@yandex.ru>
2017-01-21 19:58:01 +0200

    rope

--- js/DemoMobile.js
+++ js/DemoMobile.js
@@ -3,59 +3,57 @@
     // Matter aliases
     var Engine = Matter.Engine,
         World = Matter.World,
+        Body = Matter.Body,
         Bodies = Matter.Bodies,
+        Composite = Matter.Composite,
         Composites = Matter.Composites,
+        Constraint = Matter.Constraint,
         Common = Matter.Common,
         MouseConstraint = Matter.MouseConstraint;
 
     var Demo = {};
 
     var _engine,
-        _sceneName = 'mixed',
+        _sceneName = 'chains',
         _sceneWidth,
         _sceneHeight,
         _deviceOrientationEvent;
 
     Demo.init = function() {
-        var canvasContainer = document.getElementById('canvas-container'),
-            demoStart = document.getElementById('demo-start');
+        var canvasContainer = document.getElementById('canvas-container');
 
-        // demoStart.addEventListener('click', function() {
-        //     demoStart.style.display = 'none';
-
-            _engine = Engine.create(canvasContainer, {
-                render: {
-                    options: {
-                        wireframes: true,
-                        showAngleIndicator: true,
-                        showDebug: true
-                    }
+        _engine = Engine.create(canvasContainer, {
+            render: {
+                options: {
+                    wireframes: false,
+                    showAngleIndicator: true,
+                    showDebug: true
                 }
-            });
+            }
+        });
 
-            Demo.fullscreen();
+        _engine.world.gravity.y = 0;
 
-            setTimeout(function() {
-                var runner = Engine.run(_engine);
+        // Demo.fullscreen();
 
-                // pass through runner as timing for debug rendering
-                _engine.metrics.timing = runner;
-
-                Demo.updateScene();
-            }, 800);
-        // });
+        setTimeout(function() {
+            var runner = Engine.run(_engine);
+            // pass through runner as timing for debug rendering
+            _engine.metrics.timing = runner;
+            Demo.updateScene();
+        }, 800);
 
         window.addEventListener('deviceorientation', function(event) {
             _deviceOrientationEvent = event;
-            Demo.updateGravity(event);
+            // Demo.updateGravity(event);
         }, true);
 
-        window.addEventListener('touchstart', Demo.fullscreen);
+        // window.addEventListener('touchstart', Demo.fullscreen);
 
         window.addEventListener('orientationchange', function() {
             Demo.updateGravity(_deviceOrientationEvent);
             Demo.updateScene();
-            Demo.fullscreen();
+            // Demo.fullscreen();
         }, false);
     };
 
@@ -86,7 +84,74 @@
         
         World.add(_world, stack);
     };
-    
+
+    Demo.chains = function() {
+        var engine = _engine,
+            world = engine.world;
+
+        World.add(world, MouseConstraint.create(_engine));
+
+        group = Body.nextGroup(true);
+
+        var linkNo = 130;
+        var linkWidth = 4;
+        var linkHeight = 4;
+        var linkDistance = 2;
+        var ropeStiffness = 0.9;
+        var mountTop = {x: 160, y: -90};
+        var mountBottom = {x: 160, y: mountTop.y+linkNo*linkHeight+linkNo*linkDistance+linkDistance};
+
+        var ropeA = Composites.stack(mountTop.x, mountTop.y+linkDistance+linkHeight/2, 1, linkNo, 0, linkDistance, function (x, y) {
+            return Bodies.rectangle(x-linkWidth/2, y-linkHeight/2, linkWidth, linkHeight, {
+                //collisionFilter: {group: group},
+            });
+        });
+
+        Composites.chain(ropeA, 0, 0.5, 0, -0.5, {stiffness: ropeStiffness, length: linkDistance});
+        Composite.add(ropeA, Constraint.create({
+            bodyB: ropeA.bodies[0],
+            pointB: { x: 0, y: -linkHeight/2 },
+            pointA: mountTop,
+            length: linkDistance,
+            stiffness: ropeStiffness
+        }));
+        Composite.add(ropeA, Constraint.create({
+            bodyB: ropeA.bodies[linkNo - 1],
+            pointB: { x: 0, y: linkHeight/2 },
+            pointA: mountBottom,
+            length: linkDistance,
+            stiffness: ropeStiffness
+        }));
+
+        World.add(world, ropeA);
+
+        //ropeA.bodies[30].setPosition({ x:320, y:285 });
+        // for (i = 30; i < 50; i++) {
+        //    Matter.Body.setPosition(ropeA.bodies[i], {x: i*8.77, y: 285});
+        // }
+
+        group = Body.nextGroup(true);
+
+        var ropeB = Composites.stack(160-20, 20, 1, 5, 0, 20, function(x, y) {
+            return Bodies.circle(x, y, 20, {
+                // collisionFilter: { group: group },
+                //angle: Math.PI/2
+            });
+        });
+
+        // Composites.chain(ropeB, 0.5, 0, -0.5, 0, { stiffness: 0.8, length: 20 });
+        // Composite.add(ropeB, Constraint.create({
+        //     bodyB: ropeB.bodies[0],
+        //     pointB: { x: -20, y: 0 },
+        //     pointA: { x: 160, y: 0 },
+        //     length: 20,
+        //     stiffness: 0.8
+        // }));
+
+        // World.add(world, ropeB);
+
+    };
+
     Demo.updateScene = function() {
         if (!_engine)
             return;
@@ -107,27 +172,27 @@
         Demo[_sceneName]();
     };
     
-    Demo.updateGravity = function(event) {
-        if (!_engine)
-            return;
-        
-        var orientation = window.orientation,
-            gravity = _engine.world.gravity;
-
-        if (orientation === 0) {
-            gravity.x = Common.clamp(event.gamma, -90, 90) / 90;
-            gravity.y = Common.clamp(event.beta, -90, 90) / 90;
-        } else if (orientation === 180) {
-            gravity.x = Common.clamp(event.gamma, -90, 90) / 90;
-            gravity.y = Common.clamp(-event.beta, -90, 90) / 90;
-        } else if (orientation === 90) {
-            gravity.x = Common.clamp(event.beta, -90, 90) / 90;
-            gravity.y = Common.clamp(-event.gamma, -90, 90) / 90;
-        } else if (orientation === -90) {
-            gravity.x = Common.clamp(-event.beta, -90, 90) / 90;
-            gravity.y = Common.clamp(event.gamma, -90, 90) / 90;
-        }
-    };
+    // Demo.updateGravity = function(event) {
+    //     if (!_engine)
+    //         return;
+    //
+    //     var orientation = window.orientation,
+    //         gravity = _engine.world.gravity;
+	//
+    //     if (orientation === 0) {
+    //         gravity.x = Common.clamp(event.gamma, -90, 90) / 90;
+    //         gravity.y = Common.clamp(event.beta, -90, 90) / 90;
+    //     } else if (orientation === 180) {
+    //         gravity.x = Common.clamp(event.gamma, -90, 90) / 90;
+    //         gravity.y = Common.clamp(-event.beta, -90, 90) / 90;
+    //     } else if (orientation === 90) {
+    //         gravity.x = Common.clamp(event.beta, -90, 90) / 90;
+    //         gravity.y = Common.clamp(-event.gamma, -90, 90) / 90;
+    //     } else if (orientation === -90) {
+    //         gravity.x = Common.clamp(-event.beta, -90, 90) / 90;
+    //         gravity.y = Common.clamp(event.gamma, -90, 90) / 90;
+    //     }
+    // };
     
     Demo.fullscreen = function(){
         var _fullscreenElement = _engine.render.canvas;
