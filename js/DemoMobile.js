8f3576f40227223b2035526a87ed4ae5a97f97f2
Anton Pinchuk <antonpinchuk@yandex.ru>
2017-01-22 16:04:41 +0200

    falling objects

--- js/DemoMobile.js
+++ js/DemoMobile.js
@@ -58,22 +58,29 @@
 
     window.addEventListener('load', Demo.init);
 
-    Demo.chain = function(centerX) {
+
+    Demo.chain = function(centerX, filterGroup) {
+
+        var record;
+
+        var box;
+
         var engine = _engine,
             world = engine.world;
 
         group = Body.nextGroup(true);
 
-        var linkNo = 130;
+        var linkNo = 131;
         var linkWidth = 4;
         var linkHeight = 4;
         var linkDistance = 2;
         var ropeStiffness = 0.9;
-        var mountTop = {x: 160, y: -90};
+        var mountTop = {x: centerX, y: -90};
         var mountBottom = {x: centerX, y: mountTop.y+linkNo*linkHeight+linkNo*linkDistance+linkDistance};
 
         var ropeA = Composites.stack(mountTop.x, mountTop.y+linkDistance+linkHeight/2, 1, linkNo, 0, linkDistance, function (x, y) {
             return Bodies.rectangle(x-linkWidth/2, y-linkHeight/2, linkWidth, linkHeight, {
+                collisionFilter: { mask: filterGroup }
                 //collisionFilter: {group: group},
             });
         });
@@ -125,6 +132,9 @@
             }
         });
 
+		/**
+         * Tick
+         */
         Events.on(engine, "afterUpdate", function(event) {
 
             if (record) {
@@ -147,9 +157,14 @@
                     }
                 }
             }
-        });
 
-        var record;
+            if (!box) {
+                box = new Box();
+            } else {
+                box.tick();
+            }
+
+        });
 
         var Record = function () {
             this.isRecording = true;
@@ -180,6 +195,54 @@
                 return Math.max(Math.abs(point1.x - point2.x), Math.abs(point1.y - point2.y));
             };
         };
+        
+        var BoxShapes = [ 'square', 'rectangleVertical', 'rectangleHorizontal', 'circle', 'triangle' ];
+        
+        var Box = function() {
+            this.type = BoxShapes[Math.floor(Math.random()*BoxShapes.length)];
+            this.body = create(this.type);
+            this.body.force.y = 0.065;
+            this.body.friction.y = 0.0;
+            World.addBody(world, this.body);
+
+            var tickCount = 0;
+
+            this.tick = function() {
+                if (++tickCount == 200) {
+                    Matter.Composite.remove(world, this.body);
+                    box = null;
+                }
+                //Body.setPosition(body, body.position.x, body.position.y+1);
+            };
+
+            function create(type) {
+                var body;
+                switch (type) {
+                    case 'square':
+                        body = Bodies.rectangle(centerX, -100, 50, 50, {
+                        });
+                        break;
+                    case 'rectangleVertical':
+                        body = Bodies.rectangle(centerX, -100, 30, 80, {
+                        });
+                        break;
+                    case 'rectangleHorizontal':
+                        body = Bodies.rectangle(centerX, -100, 80, 30, {
+                        });
+                        break;
+                    case 'circle':
+                        body = Bodies.circle(centerX, -100, 30, {
+                        });
+                        break;
+                    case 'triangle':
+                        body = Bodies.polygon(centerX, -100, 3, 45, {
+                        });
+                        break;
+                }
+                return body;
+            }
+        }
+        
     };
 
 
@@ -190,8 +253,11 @@
         if (!_engine)
             return;
         
-        _sceneWidth = document.documentElement.clientWidth;
-        _sceneHeight = document.documentElement.clientHeight;
+        // _sceneWidth = document.documentElement.clientWidth;
+        // _sceneHeight = document.documentElement.clientHeight;
+        _sceneWidth = 320;
+        _sceneHeight = 570;
+
 
         var boundsMax = _engine.world.bounds.max,
             renderOptions = _engine.render.options,
@@ -203,7 +269,9 @@
         canvas.width = renderOptions.width = _sceneWidth;
         canvas.height = renderOptions.height = _sceneHeight;
 
-        Demo.chain(160);
+        Demo.chain(60,  0x0002);
+        Demo.chain(160, 0x0004);
+        Demo.chain(260, 0x0008);
     };
     
 
